bdc2113b3d8db1aaf503b2a95a51d38f8b71438f
InnerOffice <43765518+InnerOffice@users.noreply.github.com>
2018-10-04 11:50:36 -0600

    Update abilities.js

--- abilities.js
+++ abilities.js
@@ -1,2 +1,217 @@
+/*
+Ratings and how they work:
+-2: Extremely detrimental
+	  The sort of ability that relegates Pokemon with Uber-level BSTs into NU.
+	ex. Slow Start, Truant
+-1: Detrimental
+	  An ability that does more harm than good.
+	ex. Defeatist, Normalize
+ 0: Useless
+	  An ability with no net effect during a singles battle.
+	ex. Healer, Illuminate
+ 1: Ineffective
+	  An ability that has a minimal effect. Should not be chosen over any other ability.
+	ex. Damp, Shell Armor
+ 2: Situationally useful
+	  An ability that can be useful in certain situations.
+	ex. Blaze, Insomnia
+ 3: Useful
+	  An ability that is generally useful.
+	ex. Infiltrator, Sturdy
+ 4: Very useful
+	  One of the most popular abilities. The difference between 3 and 4 can be ambiguous.
+	ex. Protean, Regenerator
+ 5: Essential
+	  The sort of ability that defines metagames.
+	ex. Desolate Land, Shadow Tag
+*/
+
 'use strict';
 exports.BattleAbilities = {
+	"blaze": {
+		desc: "When this Pokemon has 1/3 or less of its maximum HP, rounded down, its attacking stat is multiplied by 1.5 while using a Fire-type attack.",
+		shortDesc: "At 1/3 or less of its max HP, this Pokemon's attacking stat is 1.5x with Fire attacks.",
+		onModifyAtkPriority: 5,
+		onModifyAtk: function (atk, attacker, defender, move) {
+			if (move.type === 'Fire') {
+				this.debug('Blaze boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA: function (atk, attacker, defender, move) {
+			if (move.type === 'Fire') {
+				this.debug('Blaze boost');
+				return this.chainModify(1.5);
+			}
+		},
+		id: "blaze",
+		name: "Blaze",
+		rating: 2,
+		num: 66,
+	},
+	"flareboost": {
+		desc: "While this Pokemon is burned, the power of its special attacks is multiplied by 1.5.",
+		shortDesc: "While this Pokemon is burned, its special attacks have 1.5x power.",
+		onDamagePriority: 1,
+		onDamage: function (damage, target, source, effect) {
+			if (effect.id === 'brn') {
+				this.heal(target.maxhp / 8);
+				return false;
+			}
+		},
+		onBasePowerPriority: 8,
+		onBasePower: function (basePower, attacker, defender, move) {
+			if (attacker.status === 'brn' && move.category === 'Special') {
+				return this.chainModify(1.5);
+			}
+		},
+		id: "flareboost",
+		name: "Flare Boost",
+		rating: 2.5,
+		num: 138,
+	},
+	"hustle": {
+		desc: "This Pokemon's Attack is multiplied by 1.5 and the accuracy of its physical attacks is multiplied by 0.8.",
+		shortDesc: "This Pokemon's Attack is 1.5x and accuracy of its physical attacks is 0.8x.",
+		// This should be applied directly to the stat as opposed to chaining witht he others
+		onModifyAtkPriority: 5,
+		onModifyAtk: function (atk) {
+			return this.modify(atk, 1.5);
+		},
+		onModifyMovePriority: -1,
+		onModifyMove: function (move) {
+			if (move.category === 'Physical' && typeof move.accuracy === 'number') {
+				move.accuracy *= 0.9;
+			}
+		},
+		id: "hustle",
+		name: "Hustle",
+		rating: 3,
+		num: 55,
+	},
+	"insomnia": {
+		shortDesc: "This Pokemon cannot fall asleep. Gaining this Ability while asleep cures it.",
+		onUpdate: function (pokemon) {
+			if (pokemon.status === 'slp') {
+				this.add('-activate', pokemon, 'ability: Insomnia');
+				this.boost({atk: 1}, source);
+			}
+		},
+		id: "insomnia",
+		name: "Insomnia",
+		rating: 2,
+		num: 15,
+	},
+	"swarm": {
+		desc: "When this Pokemon has 1/3 or less of its maximum HP, rounded down, its attacking stat is multiplied by 1.5 while using a Bug-type attack.",
+		shortDesc: "At 1/3 or less of its max HP, this Pokemon's attacking stat is 1.5x with Bug attacks.",
+		onModifyAtkPriority: 5,
+		onModifyAtk: function (atk, attacker, defender, move) {
+			if (move.type === 'Bug') {
+				this.debug('Swarm boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA: function (atk, attacker, defender, move) {
+			if (move.type === 'Bug') {
+				this.debug('Swarm boost');
+				return this.chainModify(1.5);
+			}
+		},
+		id: "swarm",
+		name: "Swarm",
+		rating: 2,
+		num: 68,
+	},
+	"overgrow": {
+		desc: "When this Pokemon has 1/3 or less of its maximum HP, rounded down, its attacking stat is multiplied by 1.5 while using a Grass-type attack.",
+		shortDesc: "At 1/3 or less of its max HP, this Pokemon's attacking stat is 1.5x with Grass attacks.",
+		onModifyAtkPriority: 5,
+		onModifyAtk: function (atk, attacker, defender, move) {
+			if (move.type === 'Grass') {
+				this.debug('Overgrow boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA: function (atk, attacker, defender, move) {
+			if (move.type === 'Grass') {
+				this.debug('Overgrow boost');
+				return this.chainModify(1.5);
+			}
+		},
+		id: "overgrow",
+		name: "Overgrow",
+		rating: 2,
+		num: 65,
+	},
+	"purepower": {
+		shortDesc: "This Pokemon's Special Attack is doubled.",
+		onModifyAtkPriority: 5,
+		onModifyAtk: function (spa) {
+			return this.chainModify(2);
+		},
+		id: "purepower",
+		name: "Pure Power",
+		rating: 5,
+		num: 74,
+	},
+	"stamina": {
+		shortDesc: "This Pokemon's Defense is raised by 1 stage after it is damaged by a move.",
+		onAfterDamage: function (damage, target, source, effect) {
+			if (effect && effect.effectType === 'Move' && effect.id !== 'confused') {
+				this.boost({def: 1});
+				this.boost({spd: 1});
+			}
+		},
+		id: "stamina",
+		name: "Stamina",
+		rating: 3,
+		num: 192,
+	},
+	"torrent": {
+		desc: "When this Pokemon has 1/3 or less of its maximum HP, rounded down, its attacking stat is multiplied by 1.5 while using a Water-type attack.",
+		shortDesc: "At 1/3 or less of its max HP, this Pokemon's attacking stat is 1.5x with Water attacks.",
+		onModifyAtkPriority: 5,
+		onModifyAtk: function (atk, attacker, defender, move) {
+			if (move.type === 'Water') {
+				this.debug('Torrent boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpAPriority: 5,
+		onModifySpA: function (atk, attacker, defender, move) {
+			if (move.type === 'Water') {
+				this.debug('Torrent boost');
+				return this.chainModify(1.5);
+			}
+		},
+		id: "torrent",
+		name: "Torrent",
+		rating: 2,
+		num: 67,
+	},
+	"toxicboost": {
+		desc: "While this Pokemon is poisoned, the power of its physical attacks is multiplied by 1.5.",
+		shortDesc: "While this Pokemon is poisoned, its physical attacks have 1.5x power.",
+		onDamagePriority: 1,
+		onDamage: function (damage, target, source, effect) {
+			if (effect.id === 'psn' || effect.id === 'tox') {
+				this.heal(target.maxhp / 8);
+				return false;
+			}
+		},
+		onBasePowerPriority: 8,
+		onBasePower: function (basePower, attacker, defender, move) {
+			if ((attacker.status === 'psn' || attacker.status === 'tox') && move.category === 'Physical') {
+				return this.chainModify(1.5);
+			}
+		},
+		id: "toxicboost",
+		name: "Toxic Boost",
+		rating: 3,
+		num: 137,
+	},
+};
