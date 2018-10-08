5c0a01a855ea5cf86ba4cc2e4846d89113856f72
InnerOffice <43765518+InnerOffice@users.noreply.github.com>
2018-10-07 18:34:10 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,5 +7,17 @@ exports.BattleScripts = {
     this.modData('Leanrsets', 'meganium').learnset.playrough = ['7M'];
     this.modData('Leanrsets', 'meganium').learnset.dazzlinggleam = ['7M'];
     this.modData('Leanrsets', 'meganium').learnset.growth = ['7M'];
+    this.modData('Learnsets', 'seviper').learnset.dragondance =['7M'];
+    this.modData('Learnsets', 'seviper').learnset.dragonpulse =['7M'];
+    this.modData('Learnsets', 'seviper').learnset.dracometeor =['7M'];
+    this.modData('Learnsets', 'seviper').learnset.dragonrush =['7M'];
+    this.modData('Learnsets', 'seviper').learnset.dragontail =['7M'];
+    this.modData('Learnsets', 'seviper').learnset.dragonhammer =['7M'];
+    this.modData('Learnsets', 'arbok').learnset.dragondance =['7M'];
+    this.modData('Learnsets', 'arbok').learnset.dragonpulse =['7M'];
+    this.modData('Learnsets', 'arbok').learnset.dracometeor =['7M'];
+    this.modData('Learnsets', 'arbok').learnset.dragontail =['7M'];
+    this.modData('Learnsets', 'arbok').learnset.dragonhammer =['7M'];
+    this.modData('Learnsets', 'arbok').learnset.dragonrush =['7M'];
   },
 };
